d4f245175b0ec50adc403f94c22a166828d57548
Richardos17 <risko.pencik@gmail.com>
2026-04-20 18:57:14 +0200

    Created basic setup for project including automated CI pipleine for backend

---
+++ backend/src/test/java/com/richardos17/family_tree/FamilyTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.richardos17.family_tree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
